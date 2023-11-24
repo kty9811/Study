e3cbd9c908dbb4adf8a60532b2d007677cf7c0fb
kangtaeyang <9811kty@naver.com>
2023-11-24 10:52:33 +0900

    11_24

--- java_basic/src/java_basic/LottoAuto.java
+++ java_basic/src/java_basic/LottoAuto.java
@@ -1,3 +1,4 @@
+//로또 번호 자동추첨기
 package java_basic;
 
 import java.util.Scanner;
@@ -6,31 +7,46 @@ public class LottoAuto {
 
 	public static void main(String[] args) {
 		
+		final int PRICE_PER_LOTTO=1000;	//로또 단가
+		
+		int[] lotto_num=new int[45];	//로또 번호
+		int[][] output_num;	//추첨번호
+		int inMoney;	//투입금액
+		int lottoCnt;//로또횟수
+		int changeMoney;//거스름돈
+		
 		Scanner scann=new Scanner(System.in);
 		
-		int inputMoney=0;	//결재금액
-		int outputMoney=0;//잔돈
-		int count=0;//로또 개수
-		int [] lottoNumber=new int[6]; //로또 번호 6개 출력
-		
 		//입력
-		System.out.print("금액을 입력하세요: ");
-		inputMoney=scann.nextInt();
-		
-		count=inputMoney/1000;	//로또 개수
-		outputMoney=inputMoney%1000;	//잔돈 반환
-		
-		
-		
-	for(int j=0; j<count; j++) {
-			for(int i=0; i<6; i++) {
-				lottoNumber[i] = (int)(Math.random() * 45+1);
-				System.out.print(lottoNumber[i]+"\t");
-		}
-		System.out.println();
-		}
-		System.out.println("잔돈은"+outputMoney+"입니다.");
+		System.out.print("금액을 입력 : ");
+		inMoney=scann.nextInt(); scann.nextLine();
+		lottoCnt=inMoney/PRICE_PER_LOTTO;
+		changeMoney=inMoney%PRICE_PER_LOTTO;
+		output_num=new int[lottoCnt][6];
 
+		//처리
+		for(int i=1; i<46; i++) {
+			lotto_num[i-1]=i;
+		}
+		for(int count=0; count<lottoCnt; count++) {
+		int temp=0;
+		for(int i=0; i<45*100; i++) {
+			int tempIndex=(int)(Math.random()*45);
+			temp=lotto_num[0];
+			lotto_num[0]=lotto_num[tempIndex];
+			lotto_num[tempIndex]=temp;
+		}
+		for(int i=0; i<6; i++) {
+			output_num[count][i]=lotto_num[i];
+		}
+		}
+		//출력
+		for(int i=0; i<output_num.length; i++) {
+			for(int j=0; j<output_num[i].length; j++) {
+				System.out.print(output_num[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		System.out.println("거스름 돈 :  ￦"+changeMoney);
 	}
-
 }
